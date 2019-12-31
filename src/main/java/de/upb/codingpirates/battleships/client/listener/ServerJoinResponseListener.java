b956782e6db63c54803ffa7e9ec43d8c637566ff
Benjamin Kasten <bkasten@mail.uni-paderborn.de>
2019-12-31 14:37:35 +0100

    Separated the functionality of the ai in multiple classes for better overview, added the MessageListener interfaces to ai class and removed AiMessageHandler.java

--- src/main/java/de/upb/codingpirates/battleships/client/listener/ServerJoinResponseListener.java
+++ src/main/java/de/upb/codingpirates/battleships/client/listener/ServerJoinResponseListener.java
@@ -2,7 +2,9 @@ package de.upb.codingpirates.battleships.client.listener;
 
 import de.upb.codingpirates.battleships.network.message.response.ServerJoinResponse;
 
+import java.io.IOException;
+
 public interface ServerJoinResponseListener extends MessageHandlerListener {
 
-    void onServerJoinResponse(ServerJoinResponse message, int clientId);
+    void onServerJoinResponse(ServerJoinResponse message, int clientId) throws IOException;
 }
